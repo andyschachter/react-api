521ce62663f06e3da64fac04604e2fcf69adf94a
Andy Schachter <andy.schachter@gmail.com>
2021-07-04 11:25:00 -0400

    updated index.js for react

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ const cors = require('cors')
 
 const app = express()
 
-app.set('view engine', 'pug')
-app.use(express.static('public'))
+app.use((req, res, next) => {
+  res.setHeader(
+    "content-security-policy-report-only",
+    "default-src 'self'; script-src 'report-sample' 'self'; style-src 'report-sample' 'self'; object-src 'none'; base-uri 'self'; connect-src 'self' https://api.github.com; font-src 'self'; frame-src 'self'; img-src 'self' https://*.githubusercontent.com; manifest-src 'self'; media-src 'self'; report-uri https://5f723390a82aebac90867ce8.endpoint.csper.io/; worker-src 'none';"
+  );
+  next();
+});
+
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use(bodyParser.json())
 
@@ -26,9 +33,11 @@ app.get('/api/beer/:identifier', getBeerByNameOrId)
 app.get('/api/style/:style', getBeerByStyle)
 app.post('/api/brewery', addNewBrewery)
 
-app.all('*', (request, response) => {
+/* app.all('*', (request, response) => {
   return response.sendStatus(404)
-})
+}) */
+
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
 
 app.listen(1344, () => {
   console.log('Listening on 1344...') // eslint-disable-line no-console
