5da0e0bfe54e569e39ef9f25128fa15e5eb01ad9
Andy Schachter <andy.schachter@gmail.com>
2021-07-04 11:58:35 -0400

    added use effects to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import ErrorPage from './components/error';
+import { fetchData, filtered } from './utils/manufacturers'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
+
+  const [breweryData, setBreweryData] = useState([])
+  const [foundBreweries, setFoundBreweries] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    async function pullData() {
+
+      const data = await fetchData()
+
+      setBreweryData(data)
+      setFoundBreweries(data)
+    }
+
+    pullData()
+  },[])
+
+  useEffect(() => {
+
+    const breweries = filtered(breweryData, searchTerm)
+
+    setFoundBreweries(breweries)
+
+  }, [searchTerm, breweryData])
+
+
   return (
     <BrowserRouter>
       <Switch>
@@ -11,7 +38,7 @@ function App() {
         <Route path="*">
           <div><ErrorPage /></div>
         </Route>
-        
+
       </Switch>
     </BrowserRouter>
   );
