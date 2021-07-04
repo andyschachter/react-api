9d4838344226841c6403adbf504d07326cfe1a85
Andy Schachter <andy.schachter@gmail.com>
2021-07-04 14:23:11 -0400

    updated css for formatting

---
+++ src/components/documentation.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Documentation = () => {
+  return (
+  <body>
+  <h1>API Documentation</h1>
+  <h4>
+    <a href='/' class='backHome'>
+      Return Home
+    </a>
+  <h3>GET /api/brewery</h3>
+  <table>
+    <tr>
+      <td>return all breweries from the API</td>
+    </tr>
+    <tr>
+      <td>
+     <pre>
+        <code>
+        </code>
+      </pre>
+      </td>
+    </tr>
+  </table>
+  </h4>
+  </body>
+  )
+}
+
+export default Documentation
